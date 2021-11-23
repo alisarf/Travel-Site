d1e3febefae5c2a681a1efa15e9176944a990031
alisarf <alisarfaresboulos@gmail.com>
2021-11-23 14:08:56 -0800

    setup tailwind within app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, Container } from '@mui/material';
 
 function App() {
   return (
     <div>
-      <Typography variant='h2' component='h1' gutterBottom>
-        My first material UI component!
-      </Typography>
+
+      <Container>
+        <Typography variant='h2' component='h1' gutterBottom className="text-center">
+          Travel Bali
+        </Typography>
+        <Typography variant='h2' component='h1' gutterBottom>
+          Explore
+        </Typography>
+        <img src="https://source.unsplash.com/random/?bali beach" alt="" />
+      </Container>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
