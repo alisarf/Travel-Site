1c975d55a19063e806c58aadd1767a923664bbf9
alisarf <alisarfaresboulos@gmail.com>
2022-01-07 11:13:40 -0800

    Video player and Head Banner hook add

--- src/pages/Denpasar.js
+++ src/pages/Denpasar.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Nav from '../material-ui/Nav'
 import Footer from '../material-ui/Footer'
+import FeatureCards from '../material-ui/FeatureCards';
+import HeadBanner from '../material-ui/HeadBanner';
 
 const Denpasar = () => {
     return (
         <div>
+            <HeadBanner title= 'Denpasar'/>
             <Nav/>
             Hello this Denpasar page
+            <FeatureCards 
+                title = 'Things to Do' 
+                subtitle = 'Explore the top tourist destinations in the Northern Region of Denpasar.' 
+            />
             <Footer/>
         </div>
     )
--- src/pages/Ubud.js
+++ src/pages/Ubud.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import Nav from '../material-ui/Nav';
-import Footer from '../material-ui/Footer'
+import Footer from '../material-ui/Footer';
+import FeatureCards from '../material-ui/FeatureCards';
+import HeadBanner from '../material-ui/HeadBanner';
+import ImgBanner from '../assets/images/ubud/ubud-banner.jpg';
+import Video from '../assets/images/bali/surf.mp4'
+import VideoBanner from '../material-ui/VideoBanner';
+
 
 const Ubud = () => {
     return (
         <div>
-            <Nav/>
-            Hello this ubud page!
+            <HeadBanner title= 'Ubud' image = {ImgBanner} />
+            <FeatureCards 
+                title = 'Things to Do' 
+                subtitle = 'Explore the top tourist destinations in the Southern Region of Ubud.' 
+            />
+            <VideoBanner 
+                video = {Video}
+                msg = 'Endless Surf'
+                speed = '1.2'
+            />
             <Footer/>
         </div>
     )
